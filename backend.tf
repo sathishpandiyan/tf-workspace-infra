d14f20e3db1c89a602586710fc9f8f2ff67d8807
sathishpandiyan <sathishpandiyan1993@gmail.com>
2025-06-21 00:39:13 +0530

    mod

--- backend.tf
+++ backend.tf
@@ -1,16 +1,23 @@
+# terraform {
+#   backend "s3" {
+#     bucket         = "sas-github-workflow"
+#     region         = "us-east-1"
+#     key            = "workspace/${terraform.workspace}/terraform.tfstate"
+#     dynamodb_table = "terraform-locks"  
+#     encrypt = true
+#   }
+#   required_version = ">=0.13.0"
+#   required_providers {
+#     aws = {
+#       version = ">= 2.7.0"
+#       source = "hashicorp/aws"
+#     }
+#   }
+# }
+
+
 terraform {
   backend "s3" {
-    bucket         = "sas-github-workflow"
-    region         = "us-east-1"
-    key            = "workspace/${terraform.workspace}/terraform.tfstate"
-    dynamodb_table = "terraform-locks"  
-    encrypt = true
-  }
-  required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source = "hashicorp/aws"
-    }
+    # Leave this empty; config will be passed via -backend-config in CLI
   }
 }
