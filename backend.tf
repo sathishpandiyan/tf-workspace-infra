5071143b6adedb51386b24dc6815b297c915b6ce
sathishpandiyan <48410825+sathishpandiyan@users.noreply.github.com>
2025-06-20 23:42:18 +0530

    adding backend file [skip ci]

---
+++ backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "sas-github-workflow"
+    region         = "us-east-1"
+    key            = "workspaceenv/${terraform.workspace}/terraform.tfstate"
+    dynamodb_table = "terraform-locks"  
+    encrypt = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
